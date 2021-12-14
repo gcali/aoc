d787d4ed40cefc9cb42dea9bdce281b3caa58ab8
gcali <gcali.git@gmail.com>
2021-12-14 07:35:47 +0100

    Still working

--- src/entries/single-entries/2021/entry/index.ts
+++ src/entries/single-entries/2021/entry/index.ts
@@ -1,4 +1,4 @@
-import { Counter } from "../../../../support/data-structure";
+import { Counter, DefaultDict } from "../../../../support/data-structure";
 import { MyIterable } from "../../../../support/sequences";
 import { entryForFile } from "../../../entry";
 
@@ -56,24 +56,36 @@ export const entry = entryForFile(
             rules[a] = b;
         }
 
-        let pairs: {[key: string]: number} = {};
+        const firstPairs = new DefaultDict<string, number>(0);
 
         for (const n of new MyIterable(start).windows(2)) {
-            pairs[n.join("")] = (pairs[n.join("")] || 0) + 1;
+            firstPairs.update(n.join(""), v => v + 1);
         }
 
+        let pairs: {[key: string]: number} = {};
+
+        for (const {key, value} of firstPairs) {
+            pairs[key] = value;
+        }
         for (let i = 0; i < 40; i++) {
-            const newPairs: {[key: string]: number} = {};
+            const newPairs = new DefaultDict<string, number>(0);
+            const my = new DefaultDict<string, number>(0);
             for (const k in pairs) {
+                my.set(k, pairs[k]);
+            }
+            for (const {key: k, value} of my) {
                 const match = rules[k];
                 if (match !== undefined) {
                     const t = k[0] + match + k[1];
                     for (const w of new MyIterable(t).windows(2)) {
-                        newPairs[w.join("")] = (newPairs[w.join("")] || 0) + pairs[k];
+                        newPairs.update(w.join(""), v => v + value);
                     }
                 }
             }
-            pairs = newPairs;
+            pairs = {};
+            for (const {key, value} of newPairs) {
+                pairs[key] = value;
+            }
         }
 
         const frequencies: {[key: string]: number} = {};
