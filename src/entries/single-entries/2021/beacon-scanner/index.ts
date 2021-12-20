3541f674b3f3facf351bee8a68b82d57a242e5e8
gcali <gcali.git@gmail.com>
2021-12-20 12:46:35 +0100

    Finally finished up day 20 2021

--- src/entries/single-entries/2021/beacon-scanner/index.ts
+++ src/entries/single-entries/2021/beacon-scanner/index.ts
@@ -1,5 +1,6 @@
 import { NotImplementedError } from "../../../../support/error";
-import { Coordinate3d, sumCoordinate } from "../../../../support/geometry";
+import { Coordinate3d, CoordinateSet, manhattanDistance, sumCoordinate } from "../../../../support/geometry";
+import { permutationGenerator, subsetGenerator } from "../../../../support/sequences";
 import { entryForFile } from "../../../entry";
 
 const xRotate = (c: Coordinate3d): Coordinate3d => ({
@@ -31,6 +32,33 @@ const combine = (...mappings: Mapping[]): Mapping => {
         return c;
     };
 };
+
+function *allMappings(): Iterable<Mapping> {
+    for (const order of permutationGenerator([0,1,2])) {
+        for (const subset of subsetGenerator([0,1,2], 0)) {
+            yield (c: Coordinate3d) => {
+                    const raw = [c.x, c.y, c.z];
+                    const ordered = order.map(k => raw[k]);
+                    const inverted = ordered.map((k,i) => subset.includes(i) ? -k : k);
+                    return {
+                        x: inverted[0],
+                        y: inverted[1],
+                        z: inverted[2]
+                    } as Coordinate3d;
+            }
+        }
+        // first = false;
+    }
+}
+
+function *brutePermutations(points: Coordinate3d[]): Iterable<{points: Coordinate3d[], mapping: Mapping}> {
+    for (const mapping of allMappings()) {
+        yield {
+            points: points.map(mapping),
+            mapping
+        };
+    }
+}
 
 function* allPermutations(points: Coordinate3d[]): Iterable<{ points: Coordinate3d[], mapping: Mapping }> {
     const actions: Mapping[] = [
@@ -106,16 +134,17 @@ export const beaconScanner = entryForFile(
         const toMatch = input.slice(1);
         while (toMatch.length > 0) {
             let found = false;
+            await outputCallback("Still to match: " + toMatch.length);
             for (const candidate of toMatch) {
-                for (const perm of allPermutations(candidate)) {
+                for (const perm of brutePermutations(candidate)) {
                     const candidatePoints = perm.points;
                     for (const candidateBeacon of candidatePoints) {
                         for (const rawBeacon of absolutePoints) {
                             const existingBeacon = deserializePoint(rawBeacon);
                             const delta = {
-                                x: candidateBeacon.x - existingBeacon.x,
-                                y: candidateBeacon.y - existingBeacon.y,
-                                z: candidateBeacon.z - existingBeacon.z
+                                x: -candidateBeacon.x + existingBeacon.x,
+                                y: -candidateBeacon.y + existingBeacon.y,
+                                z: -candidateBeacon.z + existingBeacon.z
                             };
                             const translated = candidatePoints.map((p) => sumCoordinate(p, delta));
                             const serialized = translated.map((p) => serializePoint(p));
@@ -137,6 +166,8 @@ export const beaconScanner = entryForFile(
                     }
                 }
                 if (found) {
+                    const index = toMatch.indexOf(candidate);
+                    toMatch.splice(index, 1);
                     break;
                 }
             }
@@ -149,7 +180,68 @@ export const beaconScanner = entryForFile(
 
     },
     async ({ lines, outputCallback, resultOutputCallback }) => {
-        throw new NotImplementedError();
+        const input = parseLines(lines);
+        const absolutePoints = new Set<string>(input[0].map((p) => serialize(p)));
+        const toMatch = input.slice(1);
+        const scannerPositions: Coordinate3d[] = [{x: 0, y: 0, z: 0}];
+        while (toMatch.length > 0) {
+            let found = false;
+            await outputCallback("Still to match: " + toMatch.length);
+            for (const candidate of toMatch) {
+                for (const perm of brutePermutations(candidate)) {
+                    const candidatePoints = perm.points;
+                    for (const candidateBeacon of candidatePoints) {
+                        for (const rawBeacon of absolutePoints) {
+                            const existingBeacon = deserializePoint(rawBeacon);
+                            const delta = {
+                                x: -candidateBeacon.x + existingBeacon.x,
+                                y: -candidateBeacon.y + existingBeacon.y,
+                                z: -candidateBeacon.z + existingBeacon.z
+                            };
+                            const translated = candidatePoints.map((p) => sumCoordinate(p, delta));
+                            const serialized = translated.map((p) => serializePoint(p));
+                            const overlap = serialized.filter((p) => absolutePoints.has(p)).length;
+                            if (overlap >= 12) {
+                                serialized.forEach((t) => absolutePoints.add(t));
+                                found = true;
+                            }
+                            if (found) {
+                                scannerPositions.push(delta);
+                                break;
+                            }
+                        }
+                        if (found) {
+                            break;
+                        }
+                    }
+                    if (found) {
+                        break;
+                    }
+                }
+                if (found) {
+                    const index = toMatch.indexOf(candidate);
+                    toMatch.splice(index, 1);
+                    break;
+                }
+            }
+            if (!found) {
+                throw new Error("Not found an overlap");
+            }
+        }
+
+        let bestDistance = Number.NEGATIVE_INFINITY;
+
+        await outputCallback(scannerPositions);
+
+        for (const a of scannerPositions) {
+            for (const b of scannerPositions) {
+                if (manhattanDistance(a, b) > 0) {
+                    bestDistance = Math.max(bestDistance, manhattanDistance(a, b));
+                }
+            }
+        }
+
+        await resultOutputCallback(bestDistance);
     },
     {
         key: "beacon-scanner",
