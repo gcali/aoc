66622b896c07214a993711364ff16d2b307a77cc
gcali <gcali.git@gmail.com>
2022-12-06 12:37:12 +0100

    Tweak download so it gets the current day by itself, and has an override argument

--- src/download.ts
+++ src/download.ts
@@ -1,10 +1,21 @@
 import fetch from "node-fetch";
 import fs from "fs";
 
+const arg = process.argv[2];
+
+let day = new Date().getDate();
+
+if (arg && arg.length > 0) {
+    day = parseInt(arg, 10);
+    if (day.toString() !== arg) {
+        throw new Error("Invalid input");
+    }
+}
+
 const token = fs.readFileSync("session", { encoding: "utf-8" }).split("\n")[0].trim();
 
 const main = async () => {
-    const result = await fetch("https://adventofcode.com/2022/day/5/input", {
+    const result = await fetch(`https://adventofcode.com/2022/day/${day}/input`, {
         headers: {
             cookie: `session=${token}`
         }
