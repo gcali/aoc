eb4f84988accb12b1fa721594891d25501ca1d18
gcali <gcali.git@gmail.com>
2023-01-21 21:55:50 +0100

    Add day 10 and 17

--- src/move-input.ts
+++ src/move-input.ts
@@ -1,16 +1,34 @@
 import { getLastEntry, getLastYear } from "./support/entry-list-helper";
 
 import fs from "fs";
+import { entryList } from "./entries/entryList";
 
 (() => {
-    const entry = getLastEntry();
+    let entry = getLastEntry();
 
     const year = getLastYear();
+
+    const arg = process.argv[2];
+
+    let day = new Date().getDate();
 
     if (!entry.metadata) {
         console.error("Cannot move input automatically without metadata");
         return;
     }
 
-    fs.renameSync("input.txt", `./data/${year}/${entry.metadata.key}.txt`);
+    if (arg && arg.length > 0) {
+        day = parseInt(arg, 10);
+        if (day.toString() !== arg) {
+            throw new Error("Invalid input");
+        }
+        const e = entryList[year].find(e => e.date === day)?.entry;
+        if (!e || !e.metadata) {
+            throw new Error("Could not find entry");
+        }
+        entry = e;
+    }
+
+
+    fs.renameSync("input.txt", `./data/${year}/${entry.metadata!.key}.txt`);
 })();
